07252cd8156738745e9efeb913464ee009015d61
ozielsilveira <oziel.240016@alunosatc.edu.br>
2025-05-27 19:09:41 -0300

    refactor(about): apply fluent interface to About entity and repository

--- src/domain/entities/about/About.ts
+++ src/domain/entities/about/About.ts
@@ -1,11 +1,31 @@
 import { AboutImage } from './AboutImage';
 
 export class About {
-    constructor(
+    private constructor(
         public id: number | null,
         public bodyText: string,
         public secondText: string,
         public createdAt: Date,
-        public images: AboutImage[] | null
+        public images: AboutImage[] | null = []
     ) {}
+
+    static create(bodyText: string, secondText: string, createdAt: Date): About {
+        return new About(null, bodyText, secondText, createdAt, []);
+    }
+
+    withId(id: number): About {
+        this.id = id;
+        return this;
+    }
+
+    withImages(images: AboutImage[]): About {
+        this.images = images;
+        return this;
+    }
+
+    updateTexts(bodyText?: string, secondText?: string): About {
+        if (bodyText) this.bodyText = bodyText;
+        if (secondText) this.secondText = secondText;
+        return this;
+    }
 }
